131bf9967d629deda236d4857150eff43ef8ebf7
339729139 <339729139@gotvdsb.ca>
2025-11-26 09:38:44 -0500

    done bricks

---
+++ Block.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame_Summative___Breakout
+{
+    public class Block
+    {
+        private Texture2D _texture;
+        private Rectangle _location;
+        private Color _color;
+
+        public Block(Texture2D texture, Rectangle location, Color color)
+        {
+            _texture = texture;
+            _location = location;
+            _color = color;
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _location, _color);
+        }
+
+        //public Color BrickColor
+        //{
+        //    get {  return _color; }
+        //    set { _color = value; }
+        //}
+    }
+
+}
--- Game1.cs
+++ Game1.cs
@@ -1,15 +1,28 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace MonoGame_Summative___Breakout
 {
+    enum Screen
+    {
+        Title,
+        Game,
+        Win,
+        Lose
+    }
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        Screen screen;
+        KeyboardState keyboardState;
+        Random generator = new Random();
         Texture2D blockTexture, paddleTexture, ballTexture, titleScreen;
         Rectangle window;
+        List<Block> blocks = new List<Block>();
 
         public Game1()
         {
@@ -26,13 +39,49 @@ namespace MonoGame_Summative___Breakout
             _graphics.ApplyChanges();
             // TODO: Add your initialization logic here
 
+            Color brickColor;
+
             base.Initialize();
+            for (int x = 62; x < window.Width - 100; x += 45)
+            {
+                for (int y = 25; y < window.Height / 2; y += 20)
+                {
+                    if (y - 50 < 0)
+                    {
+                        brickColor = Color.Purple;
+                    }
+                    else if (y - 50 < 40)
+                    {
+                        brickColor = Color.Blue;
+                    }
+                    else if (y - 50 < 80)
+                    {
+                        brickColor = Color.Green;
+                    }
+                    else if (y - 50 < 120)
+                    {
+                        brickColor = Color.Yellow;
+                    }
+                    else if (y - 50 < 160)
+                    {
+                        brickColor = Color.Orange;
+                    }
+                    else
+                    {
+                        brickColor= Color.Red;
+                    }
+                        blocks.Add(new Block(blockTexture, new Rectangle(x, y, 40, 15), brickColor));
+                }
+            }
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            blockTexture = Content.Load<Texture2D>("Images/rectangle (1)");
+            paddleTexture = Content.Load<Texture2D>("Images/paddle");
+            ballTexture = Content.Load<Texture2D>("Images/circle (1)");
+            titleScreen = Content.Load<Texture2D>("Images/Breakout_OG-logo");
             // TODO: use this.Content to load your game content here
         }
 
@@ -49,7 +98,12 @@ namespace MonoGame_Summative___Breakout
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            _spriteBatch.Begin();
+            for (int i = 0; i < blocks.Count; i++)
+            {
+                blocks[i].Draw(_spriteBatch);
+            }
+            _spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
